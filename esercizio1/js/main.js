c4989c3b0b38cad94c86ecff970892a0cb64f6a9
JohnQuimson <john.quimson777@gmail.com>
2023-11-09 14:41:22 +0100

    feat: array con email

--- esercizio1/js/main.js
+++ esercizio1/js/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const arrayMail = [
+  'cocaha@gmail.com',
+  'pev@tgmail.com',
+  'zer@tiplolgmail.com',
+  'fihzasu@revegmail.com',
+  'le@bigagmail.com',
+  'mekinipur@gmail.com',
+  'uraane@gogmail.com',
+  'hatgmail.com',
+  'kemigmail.com',
+  'wusdezkud@fiakgmail.com',
+  'terowzi@olgmail.com',
+  'nodom@lavgmail.com',
+  'fegmail.com',
+  'lisaoj@cugmail.com',
+  'kotgmail.com',
+  'ciczimgmail.com',
+  'gugdohe@mirazgmail.com',
+  'kovme@tugmail.com',
+  'jikoja@zargmail.com',
+  'etgmail.com',
+];
