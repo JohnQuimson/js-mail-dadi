b2345e372bf80812ad11ccf0a1f6169fa19ae7c5
JohnQuimson <john.quimson777@gmail.com>
2023-11-09 16:42:34 +0100

    feat: conclusione es1

--- esercizio1/js/main.js
+++ esercizio1/js/main.js
@@ -6,11 +6,19 @@
 const arrayMail = ['prova1', 'prova2', 'prova3', 'prova4'];
 
 const inputUtente = prompt('Inserisci la tua mail');
+let verifica = false;
 
+//Itero gli elementi dell'array
 for (let i = 0; i < arrayMail.length; i++) {
+  //verifico se input è uguale all elemento array[i]
   if (arrayMail[i] === inputUtente) {
-    console.log('La tua mail è sulla lista, alla posizione: ' + i);
-  } else {
-    console.log('La tua mail NON è in lista');
+    verifica = true;
   }
+}
+
+//Stampa
+if (verifica === true) {
+  console.log('La tua mail è in lista');
+} else {
+  console.log('La tua mail NON è in lista');
 }
